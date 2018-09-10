b0c2d15713e7f3157bf48c78cd4f40b1f212f4ea
renfx <77830749@qq.com>
2018-09-10 20:43:42 +0800

    search

--- src/components/home/RSearch.vue
+++ src/components/home/RSearch.vue
@@ -8,7 +8,6 @@
         @cancel="onCancel"
       >
       </van-search>
-	  <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
     </div>
 </template>
 
@@ -23,26 +22,6 @@
         }
       },
       methods:{
-		drawLine(){
-			// 基于准备好的dom，初始化echarts实例
-			let myChart = this.$echarts.init(document.getElementById('myChart'))
-			// 绘制图表
-			myChart.setOption({
-				xAxis: {
-					data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
-				},
-				yAxis: {},
-				series: [{
-					type: 'k',
-					data: [
-						[20, 30, 10, 35],
-						[40, 35, 30, 55],
-						[33, 38, 33, 40],
-						[40, 40, 32, 42]
-					]
-				}]
-			});
-		},
         onSearch:function(){
 
         },
